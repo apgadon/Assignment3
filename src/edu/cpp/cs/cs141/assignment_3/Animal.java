95b235b4f31704f03105c31c0f5405f7414964b6
apgadon <apgadon@gmail.com>
2017-11-19 21:08:29 -0800

    Added fields to Animal class. Nothing else.

--- src/edu/cpp/cs/cs141/assignment_3/Animal.java
+++ src/edu/cpp/cs/cs141/assignment_3/Animal.java
@@ -8,5 +8,14 @@ package edu.cpp.cs.cs141.assignment_3;
  *
  */
 public class Animal {
-
+	
+	public static enum Disease{NAME, STATUS};
+	
+	private Owner own;
+	private int age;
+	private Disease[] medicalHistory;
+	private String[] vaccinations;
+	private Appointment[] appointments;
+	
+	
 }

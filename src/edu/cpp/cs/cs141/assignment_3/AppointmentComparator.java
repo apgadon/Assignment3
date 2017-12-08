c2f18f6063431e454666162e7bf0e743eb2617d1
apgadon <apgadon@gmail.com>
2017-12-08 12:12:27 -0800

    [0.2.0] Created Comparator class. Did some shit in Database idk

---
+++ src/edu/cpp/cs/cs141/assignment_3/AppointmentComparator.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.assignment_3;
+
+import java.util.Comparator;
+
+/**
+ * @author Angela Gadon
+ *
+ */
+public class AppointmentComparator implements Comparator<Appointment>{
+	public int compare(Appointment x, Appointment y) {
+		int[] xArr = x.getDate();
+		return 0;
+	}
+}
